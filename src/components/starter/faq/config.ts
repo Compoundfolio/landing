bddb9924f707c082b535e1037eff78bdfb91ecfd
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-09-19 21:55:46 +0200

    FAQ section update

---
+++ src/components/starter/faq/config.ts
@@ -0,0 +1,37 @@
+export const FAQ_LIST = [
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: true,
+  },
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: false,
+  },
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: false,
+  },
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: false,
+  },
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: false,
+  },
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: false,
+  },
+  {
+    question: "Do you like cookies?",
+    answer: `Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own. Yes, we do, we actually produce our own.`,
+    active: false,
+  },
+]

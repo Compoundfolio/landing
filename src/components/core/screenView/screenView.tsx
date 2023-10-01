4883b57b5d021f7af663b6f300fe3dc233f016cb
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-10-01 11:55:36 +0200

    Done comparison slider interactions

--- src/components/core/screenView/screenView.tsx
+++ src/components/core/screenView/screenView.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import styles from "./screenView.module.css"
+import styles from "./screenView.module.css";
 import IntroView from "~/components/starter/intro/introView";
 import GetStarted from "~/components/starter/getStared/getStarted";
 import AskYourself from "~/components/starter/askYourself/askYourself";
@@ -12,36 +12,38 @@ import Customize from "~/components/starter/customize/customize";
 import Footer from "~/components/starter/footer/footer";
 
 export default component$(() => {
-  return <>
-    <main class={styles.screenView}>
-      <div class={styles.content} id="intro">
-        <IntroView />
-      </div>
-      <div class={styles.screenView__paralaxContainer} id="askYourself">
-        <AskYourself />
-      </div>
-      <div class={styles.content} id="getStarted">
-        <GetStarted />
-      </div>
-      <div class={styles.screenView__paralaxContainer} id="visualizations">
-        <Visualizations />
-      </div>
-      <div class={styles.screenView__paralaxContainer} id="coreFeatures">
-        <CoreFeatures />
-      </div>
-      <div class={styles.screenView__paralaxContainer} id="coreFeatures">
-        <Customize />
-      </div>
-      <div class={styles.screenView__paralaxContainer} id="featureWall">
-        <FeatureWall />
-      </div>
-      <div class={styles.content} id="numbers">
-        <Numbers />
-      </div>
-      <div class={styles.content} id="faq">
-        <Faq />
-      </div>
-      <Footer />
-    </main>
-  </>
-})
+  return (
+    <>
+      <main class={styles.screenView}>
+        <div class={styles.content} id="intro">
+          <IntroView />
+        </div>
+        <div class={styles.screenView__paralaxContainer} id="askYourself">
+          <AskYourself />
+        </div>
+        <div class={styles.content} id="getStarted">
+          <GetStarted />
+        </div>
+        <div class={styles.screenView__paralaxContainer} id="visualizations">
+          <Visualizations />
+        </div>
+        <div class={styles.screenView__paralaxContainer} id="coreFeatures">
+          <CoreFeatures />
+        </div>
+        <div class={styles.screenView__paralaxContainer} id="coreFeatures">
+          <Customize />
+        </div>
+        <div class={styles.screenView__paralaxContainer} id="featureWall">
+          <FeatureWall />
+        </div>
+        <div class={styles.content} id="numbers">
+          <Numbers />
+        </div>
+        <div class={styles.content} id="faq">
+          <Faq />
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
+});

223ebb3d85e86fe1b9843a06d9338305599cc5f5
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-10-09 10:44:32 +0200

    Done desctop and large laptop adaptivity

---
+++ src/components/starter/featureWall/const.tsx
@@ -0,0 +1,293 @@
+export const FEATURE_WALL_BRICKS = [
+  {
+    title: "Informative data",
+    description: "Track dividends and portfolio value progress",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5571)">
+          <path
+            d="M5.25 27.735L14.25 18.72L20.25 24.72L33 10.38L30.885 8.26501L20.25 20.22L14.25 14.22L3 25.485L5.25 27.735Z"
+            fill="url(#paint0_radial_1155_5571)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5571"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(18 18) rotate(90) scale(9.735 15)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5571">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Transactions",
+    description: "Form a full history of your financial dissensions",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5575)">
+          <path
+            d="M6 15.75C4.755 15.75 3.75 16.755 3.75 18C3.75 19.245 4.755 20.25 6 20.25C7.245 20.25 8.25 19.245 8.25 18C8.25 16.755 7.245 15.75 6 15.75ZM6 6.75C4.755 6.75 3.75 7.755 3.75 9C3.75 10.245 4.755 11.25 6 11.25C7.245 11.25 8.25 10.245 8.25 9C8.25 7.755 7.245 6.75 6 6.75ZM6 24.75C4.755 24.75 3.75 25.77 3.75 27C3.75 28.23 4.77 29.25 6 29.25C7.23 29.25 8.25 28.23 8.25 27C8.25 25.77 7.245 24.75 6 24.75ZM10.5 28.5H31.5V25.5H10.5V28.5ZM10.5 19.5H31.5V16.5H10.5V19.5ZM10.5 7.5V10.5H31.5V7.5H10.5Z"
+            fill="url(#paint0_radial_1155_5575)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5575"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(17.625 18) rotate(90) scale(11.25 13.875)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5575">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Projected passive income",
+    description: "Estimate, which passive income you can get in the future",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5588)">
+          <path
+            d="M11.2498 8.4L14.9998 10.5L12.8998 6.75L14.9998 3L11.2498 5.1L7.49984 3L9.59984 6.75L7.49984 10.5L11.2498 8.4ZM29.2498 23.1L25.4998 21L27.5998 24.75L25.4998 28.5L29.2498 26.4L32.9998 28.5L30.8998 24.75L32.9998 21L29.2498 23.1ZM32.9998 3L29.2498 5.1L25.4998 3L27.5998 6.75L25.4998 10.5L29.2498 8.4L32.9998 10.5L30.8998 6.75L32.9998 3ZM21.5548 10.935C20.9698 10.35 20.0248 10.35 19.4398 10.935L1.93484 28.44C1.34984 29.025 1.34984 29.97 1.93484 30.555L5.44484 34.065C6.02984 34.65 6.97484 34.65 7.55984 34.065L25.0498 16.575C25.6348 15.99 25.6348 15.045 25.0498 14.46L21.5548 10.935V10.935ZM20.0098 19.17L16.8298 15.99L20.4898 12.33L23.6698 15.51L20.0098 19.17V19.17Z"
+            fill="url(#paint0_radial_1155_5588)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5588"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(17.248 18.7519) rotate(90) scale(15.7519)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5588">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Categories",
+    description: "Group your assets as you wish",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_830_18913)">
+          <path
+            d="M18 3C9.72 3 3 9.72 3 18C3 26.28 9.72 33 18 33C26.28 33 33 26.28 33 18C33 9.72 26.28 3 18 3ZM29.895 16.5H19.5V6.105C24.915 6.78 29.22 11.085 29.895 16.5ZM6 18C6 11.895 10.59 6.84 16.5 6.105V29.895C10.59 29.16 6 24.105 6 18ZM19.5 29.895V19.5H29.895C29.22 24.915 24.915 29.22 19.5 29.895Z"
+            fill="url(#paint0_radial_830_18913)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_830_18913"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(18 18) rotate(90) scale(15)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_830_18913">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Comparison",
+    description:
+      "Compare performance of your portfolios, categories and assets",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5632)">
+          <path
+            d="M13.515 21H3V24H13.515V28.5L19.5 22.5L13.515 16.5V21ZM22.485 19.5V15H33V12H22.485V7.5L16.5 13.5L22.485 19.5Z"
+            fill="url(#paint0_radial_1155_5632)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5632"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(18 18) rotate(90) scale(10.5 15)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5632">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Automation",
+    description:
+      "Setup automatic dividend reinvestment and investment account top-ups",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5640)">
+          <path
+            d="M18 9V13.5L24 7.5L18 1.5V6C11.37 6 6 11.37 6 18C6 20.355 6.69 22.545 7.86 24.39L10.05 22.2C9.375 20.955 9 19.515 9 18C9 13.035 13.035 9 18 9ZM28.14 11.61L25.95 13.8C26.61 15.06 27 16.485 27 18C27 22.965 22.965 27 18 27V22.5L12 28.5L18 34.5V30C24.63 30 30 24.63 30 18C30 15.645 29.31 13.455 28.14 11.61V11.61Z"
+            fill="url(#paint0_radial_1155_5640)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5640"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(18 18) rotate(90) scale(16.5 12)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5640">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Goals",
+    description:
+      "Create images of your financial future and track your progress",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5644)">
+          <path
+            d="M21.6 9L21 6H7.5V31.5H10.5V21H18.9L19.5 24H30V9H21.6Z"
+            fill="url(#paint0_radial_1155_5644)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5644"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(18.75 18.75) rotate(90) scale(12.75 11.25)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5644">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+  {
+    title: "Early retirement",
+    description: "Plan your early retirement with our advanced calculator",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_1155_5636)">
+          <path
+            d="M28.5 13.5L30.375 9.375L34.5 7.5L30.375 5.625L28.5 1.5L26.625 5.625L22.5 7.5L26.625 9.375L28.5 13.5ZM17.25 14.25L13.5 6L9.75 14.25L1.5 18L9.75 21.75L13.5 30L17.25 21.75L25.5 18L17.25 14.25ZM28.5 22.5L26.625 26.625L22.5 28.5L26.625 30.375L28.5 34.5L30.375 30.375L34.5 28.5L30.375 26.625L28.5 22.5Z"
+            fill="url(#paint0_radial_1155_5636)"
+          />
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial_1155_5636"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(18 18) rotate(90) scale(16.5)"
+          >
+            <stop stop-color="#FFD391" />
+            <stop offset="1" stop-color="#FFD391" stop-opacity="0.2" />
+          </radialGradient>
+          <clipPath id="clip0_1155_5636">
+            <rect width="36" height="36" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+  },
+];
--- src/components/starter/featureWall/featureWall.tsx
+++ src/components/starter/featureWall/featureWall.tsx
@@ -25,20 +25,9 @@ export default component$(() => {
         title={`Asset management \n have never been that easy`}
       >
         <section class={[styles.featureWall, "screenViewContentMaxWidth"]}>
-          {FEATURE_WALL_BRICKS.map(({ title, description }) => (
+          {FEATURE_WALL_BRICKS.map(({ title, description, icon }) => (
             <div key={title} class={styles.featureWall__item}>
-              <svg
-                width="30"
-                height="30"
-                viewBox="0 0 30 30"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15 0C6.72 0 0 6.72 0 15C0 23.28 6.72 30 15 30C23.28 30 30 23.28 30 15C30 6.72 23.28 0 15 0ZM26.895 13.5H16.5V3.105C21.915 3.78 26.22 8.085 26.895 13.5ZM3 15C3 8.895 7.59 3.84 13.5 3.105V26.895C7.59 26.16 3 21.105 3 15ZM16.5 26.895V16.5H26.895C26.22 21.915 21.915 26.22 16.5 26.895Z"
-                  fill="white"
-                />
-              </svg>
+              {icon}
               <h3 class={styles.featureWall__item_title}>{title}</h3>
               <span class={styles.featureWall__item_description}>
                 {description}

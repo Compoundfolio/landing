31e835ebefb0e9579fccae8f33d49982ee421613
Maverick17user <maksim.epifanov.2000@mail.ru>
2023-10-14 13:32:12 +0200

    Done with adaptivity

--- src/components/starter/intro/comparisonSlider.tsx
+++ src/components/starter/intro/comparisonSlider.tsx
@@ -2,6 +2,7 @@ import { component$, useStyles$, $ } from "@builder.io/qwik";
 import styles from "./comparisonSlider.css?inline";
 import ImgExcelpng from "./img/excel.png?jsx";
 import ImgComppng from "./img/compoundPower.png?jsx";
+import AppImg from "./img/after.png?jsx";
 
 export default component$(() => {
   useStyles$(styles);
@@ -16,156 +17,145 @@ export default component$(() => {
   });
 
   return (
-    <section class="parent-container relative">
-      <div class="before-after-container">
-        <figure>
-          <div id="before_after"></div>
-        </figure>
-        <input
-          onInput$={beforeAfter}
-          onChange$={beforeAfter}
-          type="range"
-          min="0"
-          max="100"
-          value="50"
-          id="before_after_slider"
+    <>
+      <AppImg class="appImg" />
+      <section class="parent-container relative">
+        <svg
+          class="abstractFigureLeft"
+          xmlns="http://www.w3.org/2000/svg"
+          width="138"
+          height="138"
+          viewBox="0 0 138 138"
+          fill="none"
+        >
+          <rect
+            x="1"
+            y="1"
+            width="128"
+            height="127"
+            stroke="url(#paint0_linear_847_9992)"
+            stroke-width="2"
+          />
+          <rect
+            x="9"
+            y="9"
+            width="128"
+            height="128"
+            stroke="url(#paint1_linear_847_9992)"
+            stroke-width="2"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_847_9992"
+              x1="65"
+              y1="0"
+              x2="65"
+              y2="129"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#0F6F72" />
+              <stop offset="1" stop-color="#FFD391" />
+            </linearGradient>
+            <linearGradient
+              id="paint1_linear_847_9992"
+              x1="73"
+              y1="8"
+              x2="73"
+              y2="138"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#0F6F72" />
+              <stop offset="1" stop-color="#FFD391" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <svg
+          class="abstractFigureRight"
+          xmlns="http://www.w3.org/2000/svg"
+          width="138"
+          height="138"
+          viewBox="0 0 138 138"
+          fill="none"
+        >
+          <rect
+            x="1"
+            y="1"
+            width="128"
+            height="127"
+            stroke="url(#paint0_linear_847_9992)"
+            stroke-width="2"
+          />
+          <rect
+            x="9"
+            y="9"
+            width="128"
+            height="128"
+            stroke="url(#paint1_linear_847_9992)"
+            stroke-width="2"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_847_9992"
+              x1="65"
+              y1="0"
+              x2="65"
+              y2="129"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#0F6F72" />
+              <stop offset="1" stop-color="#FFD391" />
+            </linearGradient>
+            <linearGradient
+              id="paint1_linear_847_9992"
+              x1="73"
+              y1="8"
+              x2="73"
+              y2="138"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#0F6F72" />
+              <stop offset="1" stop-color="#FFD391" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <svg
+          class="cometaLine"
+          width="1919"
+          height="1"
+          viewBox="0 0 1919 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="1919" height="1" fill="url(#paint0_linear_847_10024)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear_847_10024"
+              x1="4.25368e-08"
+              y1="0.98774"
+              x2="1919"
+              y2="1"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-opacity="0" />
+              <stop offset="0.197917" stop-color="#0F6F72" />
+              <stop offset="0.802083" stop-color="#FFD391" />
+              <stop offset="1" stop-opacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <ImgComppng
+          class="beforeLogo"
+          decoding="async"
+          loading="lazy"
+          alt="Compoundfolio logo"
         />
-      </div>
-      <svg
-        class="abstractFigureLeft"
-        xmlns="http://www.w3.org/2000/svg"
-        width="138"
-        height="138"
-        viewBox="0 0 138 138"
-        fill="none"
-      >
-        <rect
-          x="1"
-          y="1"
-          width="128"
-          height="127"
-          stroke="url(#paint0_linear_847_9992)"
-          stroke-width="2"
+        <ImgExcelpng
+          class="afterLogo"
+          decoding="async"
+          loading="lazy"
+          alt="Excel logo"
         />
-        <rect
-          x="9"
-          y="9"
-          width="128"
-          height="128"
-          stroke="url(#paint1_linear_847_9992)"
-          stroke-width="2"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear_847_9992"
-            x1="65"
-            y1="0"
-            x2="65"
-            y2="129"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#0F6F72" />
-            <stop offset="1" stop-color="#FFD391" />
-          </linearGradient>
-          <linearGradient
-            id="paint1_linear_847_9992"
-            x1="73"
-            y1="8"
-            x2="73"
-            y2="138"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#0F6F72" />
-            <stop offset="1" stop-color="#FFD391" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg
-        class="abstractFigureRight"
-        xmlns="http://www.w3.org/2000/svg"
-        width="138"
-        height="138"
-        viewBox="0 0 138 138"
-        fill="none"
-      >
-        <rect
-          x="1"
-          y="1"
-          width="128"
-          height="127"
-          stroke="url(#paint0_linear_847_9992)"
-          stroke-width="2"
-        />
-        <rect
-          x="9"
-          y="9"
-          width="128"
-          height="128"
-          stroke="url(#paint1_linear_847_9992)"
-          stroke-width="2"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear_847_9992"
-            x1="65"
-            y1="0"
-            x2="65"
-            y2="129"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#0F6F72" />
-            <stop offset="1" stop-color="#FFD391" />
-          </linearGradient>
-          <linearGradient
-            id="paint1_linear_847_9992"
-            x1="73"
-            y1="8"
-            x2="73"
-            y2="138"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#0F6F72" />
-            <stop offset="1" stop-color="#FFD391" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg
-        class="cometaLine"
-        width="1919"
-        height="1"
-        viewBox="0 0 1919 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect width="1919" height="1" fill="url(#paint0_linear_847_10024)" />
-        <defs>
-          <linearGradient
-            id="paint0_linear_847_10024"
-            x1="4.25368e-08"
-            y1="0.98774"
-            x2="1919"
-            y2="1"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-opacity="0" />
-            <stop offset="0.197917" stop-color="#0F6F72" />
-            <stop offset="0.802083" stop-color="#FFD391" />
-            <stop offset="1" stop-opacity="0" />
-          </linearGradient>
-        </defs>
-      </svg>
-      <ImgComppng
-        class="beforeLogo"
-        decoding="async"
-        loading="lazy"
-        alt="Compoundfolio logo"
-      />
-      <ImgExcelpng
-        class="afterLogo"
-        decoding="async"
-        loading="lazy"
-        alt="Compoundfolio logo"
-      />
-    </section>
+      </section>
+    </>
   );
 });
